b8bf09048295343dbef6b079bf2985275c5372f0
VND ut <2251120143@ut.edu.vn>
2024-08-06 18:46:57 +0700

    fix load js

---
+++ includes/header.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Xobbee</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+    <link rel="stylesheet" href="./assets/css/base.css">
+    <link rel="stylesheet" href="./assets/css/main.css">
+    <link rel="stylesheet" href="./assets/fonts/fontawesome-free-6.6.0-web/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <script src="./assets/js/script.js" defer></script>
+
+</head>
+
+<body>
+    <div class="wrapper">
+        <!-- Header -->
+        <header class="header">
+            <div class="grid">
+                <nav class="header__navbar header__navbar-list">
+                    <div class="header__navbar-list">
+                        <div>
+                            <i class="header__logo fa-solid fa-seedling"></i>
+                        </div>
+                    </div>
+
+                    <ul class="header__navbar-list header__navbar-list-info">
+                        <li class="header__navbar-item header__navbar-item--separate">
+                            <button class="header__navbar-item-btn"><a class="header__navbar-item" href="#">TRANG
+                                    CHỦ</a></button>
+                        </li>
+                        <li class="header__navbar-item header__navbar-item--separate">
+                            <button class="header__navbar-item-btn"><a class="header__navbar-item" href="#product">SẢN
+                                    PHẨM</a></button>
+                        </li>
+                        <li class="header__navbar-item">
+                            <button class="header__navbar-item-btn">
+                                <a class="header__navbar-item" href="#">KẾT NỐI</a>
+                                <i class="fa-solid fa-chevron-down"></i>
+                                <div class="header__navbar-item-btn-list">
+                                    <div class="list-social">
+                                        <i class="header__navbar-icon fa-brands fa-facebook"></i>
+                                        <a class="header__navbar-icon-link" href="">Facebook</a>
+                                    </div>
+                                    <div class="list-social">
+                                        <i class="header__navbar-icon fa-brands fa-square-instagram"></i>
+                                        <a class="header__navbar-icon-link" href="">Instagram</a>
+                                    </div>
+                                    <div class="list-social">
+                                        <i class="header__navbar-icon fa-brands fa-tiktok"></i>
+                                        <a class="header__navbar-icon-link" href="">TikTok</a>
+                                    </div>
+                                    <div class="list-social">
+                                        <i class="header__navbar-icon fa-brands fa-youtube"></i>
+                                        <a class="header__navbar-icon-link" href="">Youtube</a>
+                                    </div>
+                                </div>
+                            </button>
+
+                        </li>
+                    </ul>
+
+                    <div class="header__navbar-list">
+                        <ul class="header__navbar-list">
+                            <div class="header__navbar-item">
+                                <input class="header__navbar-search" type="text" placeholder="Tìm kiếm...">
+                                <i class="header__navbar-search-icon fa-solid fa-magnifying-glass"></i>
+                            </div>
+                        </ul>
+
+                        <div class="header__navbar-list">
+                            <div class="header__navbar-box">
+                                <i class="header__navbar-cart fa-solid fa-bag-shopping"></i>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </header>
